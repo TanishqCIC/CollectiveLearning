e59c6c35461120e58a675b6c8be6be7495b4c88c
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-05-23 01:46:08 +0630

    Create sim_distance.py

---
+++ sim_distance.py
@@ -0,0 +1,19 @@
+import dataset as ds
+from math import sqrt
+p1='Lisa Rose'
+p2='Toby'
+
+def sim_distance(prefs, person_1, person_2):
+	si={}
+	
+	for item in prefs[person_1]:
+		if item in prefs[person_2]:
+			si[item]=1
+	# if they have no ratings in common, return 0
+	if len(si)==0: return 0
+	# Add up the squares of all the differences
+	sum_of_squares=sum([pow(prefs[person_1][item]-prefs[person_2][item],2)
+		for item in prefs[person_1] if item in prefs[person_2]])
+	return 1/(1+sum_of_squares)
+
+print('Similarity between ', p1, ' and ', p2, ' is ', sim_distance(ds.critics, p1, p2))
