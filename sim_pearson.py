08733a76081c7b6109fe6b6d7798ecb194429266
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-05-24 23:15:45 +0630

    Update sim_pearson.py

--- sim_pearson.py
+++ sim_pearson.py
@@ -3,14 +3,18 @@ from math import sqrt
 p1='Lisa Rose'
 p2='Toby'
 
-def sim_distance(prefs, person_1, person_2):
+def sim_pearson(prefs, person_1, person_2):
 	si={}
-	
+
 	for item in prefs[person_1]:
 		if item in prefs[person_2]:
 			si[item]=1
-	# if they have no ratings in common, return 0
-	if len(si)==0: return 0
+	n=len(si)
+	if n==0: return 0
+
+	sum1=sum([prefs[person_1][it] for it in si])
+	sum2=sum([prefs[person_2][it] for it in si])
+	
 	# Add up the squares of all the differences
 	sum_of_squares=sum([pow(prefs[person_1][item]-prefs[person_2][item],2)
 		for item in prefs[person_1] if item in prefs[person_2]])
