017c5cfc155d4109ea75c4b2a4caa7bb68010ed8
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-05-24 23:24:15 +0630

    Update sim_topmatches.py

--- sim_topmatches.py
+++ sim_topmatches.py
@@ -2,3 +2,12 @@ import recommendations as rs
 from math import sqrt
 p1='Lisa Rose'
 p2='Toby'
+
+def sim_distance(prefs, person_1, person_2):
+	si={}
+	
+	for item in prefs[person_1]:
+		if item in prefs[person_2]:
+			si[item]=1
+	# if they have no ratings in common, return 0
+	if len(si)==0: return 0
